a87aead7f5f5fef72aa3ee98e34e0c5d9053883f
hymerared-pixel <hymerared@gmail.com>
2026-03-24 16:47:22 +0000

    ...

--- tasks/rychkova_gauss/tests/performance/main.cpp
+++ tasks/rychkova_gauss/tests/performance/main.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
+
 #include <vector>
 
 #include "rychkova_gauss/common/include/common.hpp"
 #include "rychkova_gauss/seq/include/ops_seq.hpp"
+#include "rychkova_gauss/tbb/include/ops_tbb.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace rychkova_gauss {
@@ -40,7 +42,8 @@ TEST_P(RychkovaGaussPerf, R) {
 
 namespace {
 
-const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, RychkovaGaussSEQ>(PPC_SETTINGS_rychkova_gauss);
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, RychkovaGaussSEQ, RychkovaGaussTBB>(PPC_SETTINGS_rychkova_gauss);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
